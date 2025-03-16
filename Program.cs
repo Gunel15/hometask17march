200702a13d88868becc72431236941ed4751c899
unknown <you@example.com>
2025-03-16 00:39:50 -0700

    Methods 2: a) hazirdir

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace hometask17march
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 4, 7, 45, 32, 1, 97 };
+            int minimum = Min(arr);
+            Console.WriteLine(minimum);
+        }
+        static int Min(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                { min = arr[i]; }
+            }
+            return min;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
